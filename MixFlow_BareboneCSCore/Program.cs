6e310bf044db6d59419d46f829d8aa4922e97167
DJ Daemonix <dj.daemonix@hotmail.ca>
2018-03-16 18:38:35 +0100

    develop_bareboneCSCore init

---
+++ MixFlow_BareboneCSCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MixFlow_BareboneCSCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MixFlow_BareboneCSCore/Source/Tools/Config.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Config.cs
+/// Simple configuration Get/Set tool
+/// Copyright (c) Nodsoft ES - All Rights Reserved
+/// </summary>
+
+
+namespace Mixflow_BareboneCS.Source.Tools
+{
+	static class Config
+	{
+		public:
+		static int ReadInt(string filepath, string variable)
+		{
+			int value = 0;
+
+			return value;
+		}
+
+		static string ReadString(string filepath, string variable)
+		{
+			string value = "";
+
+
+			return value;
+		}
+	}
+}
