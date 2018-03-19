fe3b084a2e77938cc9a46e5a14d2df9dfca56641
DJ Daemonix <dj.daemonix@hotmail.ca>
2018-03-19 07:57:13 +0100

    Drafted Config System

--- MixFlow_BareboneCSCore/Source/Tools/Config.cs
+++ MixFlow_BareboneCSCore/Source/Tools/Config.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Configuration;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 /// <summary>
 /// Config.cs
@@ -12,23 +10,174 @@ using System.Threading.Tasks;
 /// </summary>
 
 
-namespace Mixflow_BareboneCSCore.Source.Tools
+namespace MixFlow_BareboneCSCore.Source.Tools
 {
-	static class Config
+    class ConfigJson
 	{
-		public static int ReadInt(string filepath, string variable)
-		{
-			int value = 0;
+		//Properties
+		public string FilePath { get; set; }
+		public string Template { get; set; }
 
-			return value;
+		//Variables
+		FileStream fileStream;
+		private string jsonRaw = null;
+		private Dictionary<String, String> dictionaryStr;
+
+		//Constructors / Deconstructor
+		ConfigJson(string fileName)
+		{
+			FilePath = fileName;
+
+			LoadConfig();
+		}
+		ConfigJson(string fileName, string template)
+		{
+			FilePath = fileName;
+			template = Template; //@TODO : Template System
+
+			LoadConfig();
+		}
+		~ConfigJson()
+		{
+			SaveConfig();
+			CloseFile();
 		}
 
-		public static string ReadString(string filepath, string variable)
+
+		//File Handling Methods
+		public void LoadConfig()
 		{
-			string value = "";
+			LoadFile();
+			Deserialize();
+		}
+		public void SaveConfig()
+		{
+			Serialize();
+			SaveFile();
+		}
+
+		protected void LoadFile()
+		{
+			try
+			{
+				fileStream = File.Open(FilePath, FileMode.OpenOrCreate);
+				jsonRaw = File.ReadAllText(@FilePath);
+			}
+			catch (Exception e)
+			{
+				System.Console.Error.WriteLine("Exception caught on {0}() : {1}", System.Reflection.MethodBase.GetCurrentMethod().Name, e);
+			}
+		}
+		protected void SaveFile()
+		{
+			try
+			{
+				File.WriteAllText(@FilePath, jsonRaw);
+			}
+			catch (Exception e)
+			{
+				System.Console.Error.WriteLine("Exception caught on {0}() : {3}", System.Reflection.MethodBase.GetCurrentMethod().Name, e);
+			}
+		}
+
+		protected void CloseFile()
+		{
+			fileStream.Close();
+		}
+
+		private void Deserialize()
+		{
+			dictionaryStr = JsonConvert.DeserializeObject<Dictionary<String, String>>(jsonRaw);
+		}
+		protected Dictionary<String, String> deserialize(string json)
+		{
+			Dictionary<string, string> dictionary = JsonConvert.DeserializeObject<Dictionary<String, String>>(json);
+			return dictionary;
+		}
+		private void Serialize()
+		{
+			jsonRaw = JsonConvert.SerializeObject(dictionaryStr, Formatting.Indented);
+		}
 
 
+			//Get Set Methods 
+		public string GetStr(string variable)
+		{
+			string str = dictionaryStr[variable];
+			return str;
+		}
+		public void GetStr(ref string parameter)
+		{
+			try {
+			parameter = dictionaryStr[nameof(parameter)];
+			}
+			catch (Exception e) {
+				System.Console.Error.WriteLine("Exception caught on {0}({1}) : {2}", System.Reflection.MethodBase.GetCurrentMethod().Name, parameter, e);
+			}
+		}
+
+		public Int32 GetInt(string variable)
+		{
+			Int32 value = 0;
+			try {
+				value = Int32.Parse(dictionaryStr[variable]);
+			}
+			catch (Exception e) {
+				System.Console.Error.WriteLine("Exception caught on {0}({1}) : {2}", System.Reflection.MethodBase.GetCurrentMethod().Name, variable, e);
+			}
 			return value;
+		}
+		public void GetInt(ref Int32 parameter)
+		{
+			try {
+				parameter = Int32.Parse(dictionaryStr[nameof(parameter)]);
+			}
+			catch (Exception e) {
+				System.Console.Error.WriteLine("Exception caught on {0}({1}) : {2}", System.Reflection.MethodBase.GetCurrentMethod().Name, parameter, e);
+			}
+		}
+
+		public void SetStr(string variable, string value)
+		{
+			try {
+				dictionaryStr[variable] = value;
+			}
+			catch (Exception e)
+			{
+				System.Console.Error.WriteLine("Exception caught on {0}({1},{2}) : {3}", System.Reflection.MethodBase.GetCurrentMethod().Name, variable, value, e);
+			}
+		}
+		public void SetStr(ref string parameter)
+		{
+			try {
+				dictionaryStr[nameof(parameter)] = parameter;
+			}
+			catch (Exception e)
+			{
+				System.Console.Error.WriteLine("Exception caught on {0}({1}) : {2}", System.Reflection.MethodBase.GetCurrentMethod().Name, parameter, e);
+			}
+		}
+
+		public void SetInt(string variable, int value)
+		{
+			try {
+				dictionaryStr[variable] = value.ToString();
+			}
+			catch (Exception e)
+			{
+				System.Console.Error.WriteLine("Exception caught on {0}({1},{2}) : {3}", System.Reflection.MethodBase.GetCurrentMethod().Name, variable, value, e);
+			}
+		}
+		public void SetInt(ref int parameter)
+		{
+			try {
+				dictionaryStr[nameof(parameter)] = parameter.ToString();
+			}
+			catch (Exception e)
+			{
+				System.Console.Error.WriteLine("Exception caught on {0}({1}) : {2}", System.Reflection.MethodBase.GetCurrentMethod().Name, parameter, e);
+			}
+
 		}
 	}
 }
