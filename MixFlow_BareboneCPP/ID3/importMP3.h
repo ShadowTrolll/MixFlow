1af9e6c8c81f20fdf4b50e6ed714b5cac5b278b1
DJ Daemonix <dj.daemonix@hotmail.ca>
2018-04-19 13:12:18 +0200

    Trim BareboneCPP

--- MixFlow_BareboneCPP/ID3/importMP3.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <taglib\taglib.h>
-#include <taglib\mpegfile.h>
-#include <taglib\fileref.h>
-
-using namespace TagLib;
-
-TagLib::FileRef fileMP3("Resources_Test/DJ Daemonix - Axiom (Original Mix).mp3");
-TagLib::String artist = fileMP3.tag()->artist();
--- MixFlow_BareboneCPP/sqlite_io.cpp
+++
@@ -1,9 +0,0 @@
-#include "sqlite_io.h"
-
-/**
-* SQLite_IO.cpp
-* Simple SQLite IO Controller
-* Copyright (c) 2018 Nodsoft ES, All Rights Reserved
-**/
-
-
