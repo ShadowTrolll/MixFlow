7d6bd9e3b49a690adae2083d56081cc49af2481b
DJ Daemonix <dj.daemonix@hotmail.ca>
2018-04-22 02:14:14 +0200

    Add ID3 Interfaces
    I_ID3Handle still under WIP.

---
+++ ID3x/v2/I_ID3Driver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using static MixFlow.Utilities.Utilities;
+
+namespace MixFlow.ID3v2
+{
+	interface I_ID3Driver
+	{
+		//Properties
+		FileInfo MusicFileInfo { get; }
+
+		Dictionary<string, object> FramesDictionary { get; set; }
+
+
+		//Methods
+			//Frame Ops
+		object ReadFrame(string frame);
+
+		void SaveFrame(string frame, object value);
+		void SaveFrame(Frame frame);
+
+		void ImportFramesFromDictionary(Dictionary<string, object> framesDictionary);
+
+		Dictionary<string, object> ExportFramesToDictionary();
+
+		//File Ops
+		void PushFramesToFile(FileHandleModes fileHandleModes);
+		void PullFramesFromFile(FileHandleModes fileHandleModes);
+	}
+}
---
+++ ID3x/v2/I_ID3Handle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MixFlow.ID3v2
+{
+	interface I_ID3Handle
+	{
+		//Variables
+		int DatabaseTrackID { get; }
+	}
+}
