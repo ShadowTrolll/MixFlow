b6efc0c515a0e5ca38714b658a08e012e96eb532
DJ Daemonix <dj.daemonix@hotmail.ca>
2018-04-22 02:13:22 +0200

    Move I_SQLiteHandle to separate file

--- SQLite_Handle/DB_Handle.cs
+++ SQLite_Handle/DB_Handle.cs
@@ -6,16 +6,6 @@ using System.Data.SQLite;
 
 namespace MixFlow.SQLite_Handle
 {
-	public interface I_SQLiteHandle
-	{
-		string DatabasePath { get; set; }
-
-
-		void OpenDatabase(FileMode fileMode = FileMode.OpenOrCreate);
-
-		void Transaction(string transactionNonQuery);
-		void Transaction(string transactionQuery, out SQLiteDataReader dataOut);
-	}
 
 	public class DBHandle : I_SQLiteHandle
 	{
---
+++ SQLite_Handle/I_SQLiteHandle.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Data.SQLite;
+
+namespace MixFlow.SQLite_Handle
+{
+	public interface I_SQLiteHandle
+	{
+		string DatabasePath { get; set; }
+
+
+		void OpenDatabase(FileMode fileMode = FileMode.OpenOrCreate);
+
+		void Transaction(string transactionNonQuery);
+		void Transaction(string transactionQuery, out SQLiteDataReader dataOut);
+	}
+}
